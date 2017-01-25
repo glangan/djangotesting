595228a6293e6f2451c61282db05cb898f212704
Gaurav Langan <gauravlangan@gmail.com>
2017-01-25 11:46:15 +0800

    URLs for form, moved code in views + tests

--- lists/views.py
+++ lists/views.py
@@ -6,13 +6,14 @@ from .models import Item
 
 
 def home_page(request):
-    if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-        return redirect('/lists/the_only_list_in_the_world')
     return render(request, 'home.html')
 
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the_only_list_in_the_world/')
