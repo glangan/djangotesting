ceeec6759f6a55464a3c9e6cc7a5b01ebfad2674
Gaurav Langan <gauravlangan@gmail.com>
2017-01-25 10:18:56 +0800

    Second user test added to functional tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -38,6 +38,8 @@ class NewVisitorTest(LiveServerTestCase):
         # When user hits enter, page updates, and now page lists
         # "1. Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
+        user_list_url = self.browser.current_url
+        self.assertRegex(user_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1. Buy peacock feathers')
 
         # There is still a text box to fill in a second item
@@ -49,6 +51,33 @@ class NewVisitorTest(LiveServerTestCase):
         # The page updates again, and now shows both items in the list
         self.check_for_row_in_list_table('1. Buy peacock feathers')
         self.check_for_row_in_list_table('2. Use peacock feathers to make a fly')
+
+        # Another user comes along to the site.
+        # We use a new browser session to make sure there is no information
+        # os previous user coming through from cookies
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # User visits the home page. There is no sign of list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # User starts a new list by entering a new item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Second user gets his own unique url
+        second_list_url = self.browser.current_url
+        self.assertRegex(second_list_url, '/lists/.+')
+        self.assertNotEqual(second_list_url, user_list_url)
+
+        # Again, there is no trace of first user list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
 
         # More tests to come
         self.fail('Finish the test!')
